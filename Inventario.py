f683f6a53072c7312c6cb5fee0fb8e74758229fd
yulianisrivera51-crypto <yulianisrivera51@gmail.com>
2026-03-27 10:43:24 -0500

    Update-Inventario.py

---
+++ Inventario.py
@@ -0,0 +1,55 @@
+print("Bienvenido a el inventario.")
+
+# calculate quantity and prince for the total of the product
+def calculoTotalProducto (cantidad, precio ):
+    resultado = cantidad * precio
+    return resultado
+
+# request user data
+validarProducto = True
+while validarProducto == True:
+    nombreProducto = input("Ingresa el nombre del Producto: ").strip()
+
+    if nombreProducto and not nombreProducto.isdigit():
+        validarProducto = False
+
+    elif nombreProducto.isdigit():
+        print("Error: no se permiten solo nombres en el inventario.")
+    
+    else:
+        print("Error: el nombre del producto no puede estar vacio.")
+
+validarPrecio = True  
+while validarPrecio:
+    try:
+        precio = float(input("Ingresa el precio del producto: "))
+
+        if precio > 0:
+            validarPrecio = False
+
+        else:
+            print("Error: el precio del producto es positivo.")
+
+    except ValueError:
+        print("Error: porfavor ingrese un numero valido")
+
+validarCantidad = True
+while validarCantidad:
+    try:
+        cantidad = int(input("Ingresa la cantidad del producto: "))
+
+        if cantidad > 0:
+            validarCantidad = False
+
+        else:
+            print("Error: la cantidad del producto es positivo.")
+
+    except ValueError:
+        print("Error: porfavor ingrese un numero valido")
+
+# show data
+print(f"""
+producto: {nombreProducto}
+precio: {precio}
+cantidad: {cantidad}
+Costo Total: {calculoTotalProducto(cantidad, precio)}""")
